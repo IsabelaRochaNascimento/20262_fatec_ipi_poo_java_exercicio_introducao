f160dde88f3c648f4af44ecc7fb41fa49e1f30a1
Isabela Rocha Do Nascimento <isabela.rochadonascimento@gmail.com>
2026-08-27 11:55:47 -0300

    Update Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,18 +1,18 @@
-import javax.swing.JOptionPane; //ESTUDAR E REVER E COMENTAR
+import javax.swing.JOptionPane; //Import = traz a classe JOptionPane
 
-public class Exercicio3{
-    public static void main(String args []){
-        double prova = Double.parseDouble(
+public class Exercicio3{    //Declaração da classe principal
+    public static void main(String args []){    //ponto de entrada do programa
+        double prova = Double.parseDouble(    //pega essa String retornada NA HORA e já CONVERTE para double
             JOptionPane.showInputDialog("Nota de prova: ")//conversao e captura de uma vez so
         );
-        double trabalho = Double.parseDouble(
-            JOptionPane.showInputDialog("Notab de trabalho: ")
+        double trabalho = Double.parseDouble(    //pede a nota do trabalho e já converte direto para double
+            JOptionPane.showInputDialog("Nota de trabalho: ")
         );
 
-        double media = (prova * 6 + trabalho * 4) / 10;
+        double media = (prova * 6 + trabalho * 4) / 10;    //calculo
 
         JOptionPane.showMessageDialog(
-            null,
+            null,    //sem janela "pai", a caixa aparece centralizada.
             "Media ponderada: " + media
         );
     }
