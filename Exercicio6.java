5a96504c77acc7fe8738231405dc13c25e7f5768
Isabela Rocha Do Nascimento <isabela.rochadonascimento@gmail.com>
2026-08-27 22:59:23 -0300

    Update Exercicio6.java

--- Exercicio6.java
+++ Exercicio6.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+import java.util.Scanner; //importa a classe que nesse caso é o Scanner 
 
 public class Exercicio6 {
-    public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("Digite um valor de 1 a 7: ");
-        int numero = leitor.nextInt();
-
+    public static void main(String[] args) { //public -> acessível de fora | static -> pertence à classe
+        Scanner leitor = new Scanner(System.in); //new -> cria o objeto qeu vai capturar oq o usuario vai digitar
+       //leitor - nome que escolhemos para a variavel //(System.in) -> entrada padrao do sistema 
+        System.out.println("Digite um valor de 1 a 7: "); //println ele ja pula a linha no final
+        int numero = leitor.nextInt(); //guarda o valor retornado na variável numero do tipo int
+                                    //chama o método nextInt() sobre o objeto leitor
         //fall-through
         switch(numero){
             case 1: System.out.println("Segunda"); break;
@@ -16,8 +17,9 @@ public class Exercicio6 {
             case 6: System.out.println("Sabado"); break;
             case 7: System.out.println("Domingo"); break;
             default: System.out.println("Opcao Invalida"); break;
+                //break necessario pq em java ele nao para de executar um case
         }
-        System.out.println("Até mais");
-        leitor.close();
-    }
+        System.out.println("Até mais"); //sempre sera executada por estar fora das chaves
+        leitor.close(); //boa pratica para sempre fechar recursos
+    } 
 }
